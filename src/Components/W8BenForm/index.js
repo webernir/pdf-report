2e7838268f4ced8d1a2fcf0a3b0f5467a74aaafe
Nir Weber <webernir@bina-fin.co.il>
2017-05-28 09:23:53 +0300

    some enhancements

--- src/Components/W8BenForm/index.js
+++ src/Components/W8BenForm/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import template from '../../Templates/w8ben.png'
+import template from './w8ben.png'
+import SignatureCanvas from 'react-signature-canvas'
 import './style.css'
 
-const FieldElement = ({value, style}) => {
-    console.log(style)
-console.log(FieldElement.defaultProps)
+const FieldElement = ({ value, style }) => {
     return (
-        <div style={style}>
+        <div style={{ ...FieldElement.defaultProps.style, ...style }}>
             {value}
         </div>
     )
@@ -18,20 +17,30 @@ FieldElement.defaultProps = {
         left: '0px',
         top: '0px',
         lineHeight: '28px',
-        verticalAlign: 'bottom'
+        verticalAlign: 'bottom',
+        color: 'blue',
+        // border: '1px solid grey'
     }
 }
 
 const W8BenForm = () => {
-    const nameStyle = {
-        style: {
-            top: '138px'
-        }
-    }
     return (
         <div>
-            <img className='baseImg' alt='w8ben' src={template}/>
-            < FieldElement value='Nir Weber' />
+            <img className='baseImg' alt='w8ben' src={template} />
+            <FieldElement value='Moshe Davidovitch' style={{ top: '468px', left: '138px' }} />
+            <FieldElement value='Israel' style={{ top: '468px', left: '865px' }} />
+            <FieldElement value='Raul Velenberg 20, Ramat Ha-Hayal, Shuk Tzafon' style={{ top: '590px', left: '138px' }} />
+            <FieldElement value='Tel Aviv' style={{ top: '638px', left: '138px' }} />
+            <FieldElement value='Israel' style={{ top: '638px', left: '922px' }} />
+            <FieldElement value='32165497' style={{ top: '786px', left: '138px' }} />
+            <FieldElement value='32165497' style={{ top: '786px', left: '806px' }} />
+            <FieldElement value='28/05/2017' style={{ top: '1490px', left: '806px' }} />
+            <div style={{top:'1472px', position:'absolute', left: '234px'}}>
+                <SignatureCanvas 
+                    penColor='blue'
+                    canvasProps={{width: 528, height: 42, className: 'sigCanvas'}} 
+                    />
+    </div>
         </div>
     );
 };
